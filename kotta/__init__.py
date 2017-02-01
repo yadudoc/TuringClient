cc48567f36dd89a31decbde2e5fad68b0071cad9
Yadu Nand B <yadudoc1729@gmail.com>
2017-02-01 20:48:50 +0000

    Adding logging code. Following boto style

--- kotta/__init__.py
+++ kotta/__init__.py
@@ -1,5 +1,38 @@
 from kotta.kotta import Kotta
 from kotta.kotta_job import KottaJob
 from kotta.kotta_functions import *
+import logging
+
+__author__  = 'Yadu Nand Babuji'
+__version__ = '0.1.0'
 
 __all__ = [Kotta, KottaJob, KottaFn, kottajob]
+
+
+def set_stream_logger(name='kotta', level=logging.DEBUG, format_string=None):
+    ''' Add a stream log handler
+    '''
+
+    if format_string is None:
+        format_string = "%(asctime)s %(name)s [%(levelname)s] %(message)s"
+          
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    formatter = logging.Formatter(format_string)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+            
+
+class NullHandler (logging.Handler):
+    ''' Setup default logging to /dev/null since this is library.
+    '''
+
+    def emit(self, record):
+        pass
+
+
+    
+logging.getLogger('kotta').addHandler(NullHandler())
+    
